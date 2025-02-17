02dc890e47d4a440af40edde3571693f1014b81d
EmmanuelPons <151550471+EmmanuelPons@users.noreply.github.com>
2025-02-17 15:24:24 +0100

    Add files via upload

---
+++ src/test_DH22/test_DH22.ino
@@ -0,0 +1,28 @@
+#include "DHT.h"
+#include <MKRWAN.h>
+
+// Broche DHT
+#define DHTPIN 16 // D16
+// Definit le type de capteur utilise
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+
+void setup() {
+  // put your setup code here, to run once:
+    Serial.begin(9600);
+    dht.begin();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  String TemperatureValue = String(dht.readTemperature());
+  String HumidityValue = String(dht.readHumidity());
+
+  Serial.println("Temperature = " + TemperatureValue +" °C");
+  Serial.println("Humidite = " + HumidityValue +" %");
+  // Attend 5 secondes avant de reboucler
+  delay(1000);
+
+}
